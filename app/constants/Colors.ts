11cd45929f5ea9b9acf813c0e204536876a85a7d
Hoa0304 <chtran030404@gmail.com>
2024-05-09 09:10:33 +0700

    update

---
+++ app/constants/Colors.ts
@@ -0,0 +1,15 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+export const Colors = {
+  primary: '#17C2EC',
+  secondary: '#000000',
+  third: '#F5F5F5',
+  backgroud: '#0B153C',
+  text: '#BFBFBF',
+  input: '#113F6F',
+  white: '#FFFFFF',
+  checkbox: '#E0E0E0',
+};
